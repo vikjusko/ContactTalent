f27569ba14057e0d0b8a98fdf2a88749e7c6280f
Victoria Jusko <vikjusko@gmail.com>
2021-01-24 12:12:10 +0000

    Added styles and layouts to app

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require './lib/talent_tool'
 
 class TalentBase < Sinatra::Base
   enable :sessions, :method_override
+	set :public_folder, File.expand_path("../../public", __FILE__)
 
   get '/' do
     erb :index
