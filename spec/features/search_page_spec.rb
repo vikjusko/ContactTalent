f09ed9dcafefa7930ef8605ed38eb6bdc468b7e2
Victoria Jusko <vikjusko@gmail.com>
2021-01-24 12:23:16 +0000

    The website does bring up the name of matching talent

--- spec/features/search_page_spec.rb
+++ spec/features/search_page_spec.rb
@@ -3,4 +3,11 @@ feature "search page spec" do
 		visit '/search'
 		expect(page).to have_content("Please insert a location - city (no abbreviations please) to find the talent you are looking for")
 	end
+
+	it "it finds the user that lives in a submitted city " do
+		visit "/search"
+		fill_in "location", with: "Seattle"
+		click_button "Search"
+		expect(page).to have_content("Morty Smith")
+	end
 end 
